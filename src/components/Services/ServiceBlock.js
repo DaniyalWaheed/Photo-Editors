2e404770d0c6354218ab948debdcf208d74288fb
Daniyal Waheed <muhammad.daniyal@synavos.com>
2022-12-26 00:37:18 +0500

    hover effect added

--- src/components/Services/ServiceBlock.js
+++ src/components/Services/ServiceBlock.js
@@ -3,14 +3,15 @@ import styles from "./styles.module.css";
 
 const ServiceBlock = ({ logo, title, description }) => {
   return (
-    <div className="w-auto p-7 cursor-pointer">
-      <div className="bg-gray-103 p-6 rounded-lg">
+    <div className="w-auto p-7 cursor-pointer group relative">
+      <div className="bg-gray-103 p-6 relative w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-10 lg:aspect-w-1">
         <img
           loading="lazy"
           className="h-52 rounded w-full object-cover object-center mb-6"
           src={logo}
           alt="content"
         />
+
         <h0 className="tracking-widest text-indigo-500 font-medium title-font text-2xl">
           {title}
         </h0>
